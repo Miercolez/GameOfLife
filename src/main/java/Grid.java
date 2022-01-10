2621dd0228da9c089cc705ea9ee6d9fc848b1701
Jonas Fredriksson <48363473+Miercolez@users.noreply.github.com>
2022-01-10 15:24:13 +0100

    GridTest add_cell_to_array_returns_cell_isAlive_true - Cell added to array and method getCellFromList returns Cell

--- src/main/java/Grid.java
+++ src/main/java/Grid.java
@@ -2,17 +2,23 @@ public class Grid {
 
     private final int rows;
     private final int columns;
+    private final Cell[][] cells;
 
     public Grid(int rows, int columns) {
         this.rows = rows;
         this.columns = columns;
+        this.cells = new Cell[rows][columns];
     }
 
     public int size() {
         return rows * columns;
     }
 
-    public Cell addCell(int row, int column) {
-        return new Cell(row, column);
+    public void addCell(int row, int column) {
+        this.cells[row - 1][column - 1] =  new Cell(row, column);
+    }
+
+    public Cell getCellFromList(int row, int column) {
+        return this.cells[row - 1][column - 1];
     }
 }
--- src/test/java/GridTest.java
+++ src/test/java/GridTest.java
@@ -1,25 +1,31 @@
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class GridTest {
 
     @Test
-    void add_cell_returns_cell_isAlive_true() {
+    void add_cell_to_array_returns_cell_isAlive_true() {
         Grid grid = new Grid(4,8);
 
-        Cell cell = grid.addCell(2, 4);
+        grid.addCell(2,4);
+        Cell cell = grid.getCellFromList(2,4);
 
-        assertEquals(true, cell.isAlive());
+        assertTrue(cell.isAlive());
     }
 
     @Test
     void add_cell_row_2_column_4_returns_cell_row_2_column_4() {
         Grid grid = new Grid(4,8);
 
-        Cell cell = grid.addCell(2,4);
+        grid.addCell(2,4);
+
+        Cell cell = grid.getCellFromList(2,4);
 
         assertEquals(2, cell.row());
         assertEquals(4, cell.column());
     }
+
 }
