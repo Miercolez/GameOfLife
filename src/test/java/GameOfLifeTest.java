e368069bec42e678ff3f344eb8cf76d8178fa28d
Jonas Fredriksson <48363473+Miercolez@users.noreply.github.com>
2022-01-10 13:26:17 +0100

    GameOfLifeTest create_grid_6_by_8_returns_48 - created GameOfLife class.

---
+++ src/test/java/GameOfLifeTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+
+public class GameOfLifeTest {
+
+    @Test
+    void create_grid_6_by_8_returns_48() {
+        GameOfLife gameOfLife = new GameOfLife();
+    }
+}
