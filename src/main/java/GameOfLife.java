ae9009d0be42dc119a54e969e17960298a2311b0
Jonas Fredriksson <48363473+Miercolez@users.noreply.github.com>
2022-01-10 13:29:38 +0100

    GameOfLifeTest create_grid_6_by_8_returns_48 - created method in GameOfLife that returns 48

--- src/main/java/GameOfLife.java
+++ src/main/java/GameOfLife.java
@@ -1,2 +1,6 @@
 public class GameOfLife {
+
+    public int createGrid(int rows, int columns) {
+        return 48;
+    }
 }
--- src/test/java/GameOfLifeTest.java
+++ src/test/java/GameOfLifeTest.java
@@ -5,5 +5,7 @@ public class GameOfLifeTest {
     @Test
     void create_grid_6_by_8_returns_48() {
         GameOfLife gameOfLife = new GameOfLife();
+
+        int actual = gameOfLife.createGrid(6,8);
     }
 }
