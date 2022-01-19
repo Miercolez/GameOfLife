7a22d31aa7309b829d3c2b69ada9f372728c70b9
Jonas Fredriksson <48363473+Miercolez@users.noreply.github.com>
2022-01-19 10:54:21 +0100

    Change GameOfLife from class to record.

--- src/main/java/gameoflife/GameOfLife.java
+++ src/main/java/gameoflife/GameOfLife.java
@@ -1,12 +1,6 @@
 package gameoflife;
 
-public class GameOfLife {
-
-    private final Grid grid;
-
-    public GameOfLife(Grid grid) {
-        this.grid = grid;
-    }
+public record GameOfLife(Grid grid) {
 
     public Grid getGrid() {
         return grid;
